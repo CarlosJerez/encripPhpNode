fd3c94b8e4eca2aa61d70bf51940f9a2651c9275
Carlos Jerez <carlosjerezquintero@gmail.com>
2024-04-14 11:50:58 -0400

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "nodePhp",
+    script : "./src/index.js",
+    env: {
+        PORT:3000,
+        AUTENTIC:2554445522,
+        LUNA_ESTINO:584166227839,
+        LUNA_KEYBANK:'A11103402525120190822HB01',
+    }
+  }]
+}
